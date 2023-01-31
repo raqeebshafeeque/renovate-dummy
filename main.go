fd416e91c60dd56d8940ce1bdbc5c8aa0d9b1d34
raqeebshafeeque <raqeebshafeequekk@gmail.com>
2023-01-31 22:23:23 +0530

    Add some dependency

--- main.go
+++ main.go
@@ -6,11 +6,28 @@ import (
 	"net/http"
 	"renovatedummy/maths"
 	"renovatedummy/server"
+
+	"github.com/sanity-io/litter"
 )
 
 func main() {
 	sum := maths.Add(5, 5)
 	fmt.Printf("5 + 5: %d\n", sum)
+
+	type Person struct {
+		Name   string
+		Age    int
+		Parent *Person
+	}
+
+	litter.Dump(Person{
+		Name: "Bob",
+		Age:  20,
+		Parent: &Person{
+			Name: "Jane",
+			Age:  50,
+		},
+	})
 
 	http.HandleFunc("/double", server.DoubleHandler)
 	err := http.ListenAndServe(":8080", nil)
